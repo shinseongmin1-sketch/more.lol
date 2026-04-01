fa2ba49bf905c7acc83ce1f02df405266d792e77
seongmin <shinseongmin1@daum.net>
2026-04-01 20:37:13 +0900

    auto: 2026-04-01 20:37

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import logoImg from '../assets/logo.png'
 import {
   incrementSummonerSearch,
   getTodaySummonerCount,
