4b5846eb26d3969fb081253392da12ce6a5b6cd6
seongmin <shinseongmin1@daum.net>
2026-04-01 20:58:28 +0900

    auto: 2026-04-01 20:58

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import { useNavigate, useLocation, Link } from 'react-router-dom'
 import type { User } from '../utils/auth'
-import logoImg from '../assets/logo.png'
 import './Header.css'
 
 interface HeaderProps {
